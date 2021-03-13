32234b9f728de0c55e70d7ab36d2407f2c557e76
Song <stalker5217@gmail.com>
2021-03-13 23:00:05 +0900

    입력 값 제한하기

--- src/main/java/com/example/restexample/events/EventController.java
+++ src/main/java/com/example/restexample/events/EventController.java
@@ -1,5 +1,6 @@
 package com.example.restexample.events;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -15,13 +16,17 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 @RequestMapping(value = "/api/events", produces = MediaTypes.HAL_JSON_VALUE)
 public class EventController {
     private final EventRepository eventRepository;
+    private final ModelMapper modelMapper;
 
-    public EventController(EventRepository eventRepository){
+    public EventController(EventRepository eventRepository, ModelMapper modelMapper){
         this.eventRepository = eventRepository;
+        this.modelMapper = modelMapper;
     }
 
     @PostMapping
-    public ResponseEntity<Event> createEvent(@RequestBody Event event){
+    public ResponseEntity<Event> createEvent(@RequestBody EventDto eventDto){
+        Event event = modelMapper.map(eventDto, Event.class);
+
         Event newEvent = this.eventRepository.save(event);
 
         URI createdUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
