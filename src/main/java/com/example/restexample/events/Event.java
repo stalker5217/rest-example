bea4488ab121eafa7f6b00a73199f313a9b91ba1
Song <stalker5217@gmail.com>
2021-03-13 22:39:40 +0900

    Event Repository

--- src/main/java/com/example/restexample/events/Event.java
+++ src/main/java/com/example/restexample/events/Event.java
@@ -2,6 +2,7 @@ package com.example.restexample.events;
 
 import lombok.*;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Builder
@@ -10,7 +11,10 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @EqualsAndHashCode(of="id")
+@Entity
 public class Event {
+
+    @Id @GeneratedValue
     private Integer id;
     private String name;
     private String description;
@@ -22,9 +26,10 @@ public class Event {
     private int basePrice; // (optional)
     private int maxPrice; // (optional)
     private int limitOfEnrollment;
-
     private boolean offline;
     private boolean free;
+
+    @Enumerated(EnumType.STRING)
     private EventStatus eventStatus = EventStatus.DRAFT;
 }
 
--- src/main/java/com/example/restexample/events/EventController.java
+++ src/main/java/com/example/restexample/events/EventController.java
@@ -14,10 +14,17 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 @Controller
 @RequestMapping(value = "/api/events", produces = MediaTypes.HAL_JSON_VALUE)
 public class EventController {
+    private final EventRepository eventRepository;
+
+    public EventController(EventRepository eventRepository){
+        this.eventRepository = eventRepository;
+    }
+
     @PostMapping
     public ResponseEntity<Event> createEvent(@RequestBody Event event){
-        URI createdUri = linkTo(EventController.class).slash("{id}").toUri();
-        event.setId(10);
+        Event newEvent = this.eventRepository.save(event);
+
+        URI createdUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
         return ResponseEntity.created(createdUri).body(event);
     }
 }
