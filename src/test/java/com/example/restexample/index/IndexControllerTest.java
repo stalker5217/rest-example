fcbde9c165ca451023740e9163d4845adc6eaa36
Song <stalker5217@gmail.com>
2021-06-29 11:25:00 +0900

    테스트 코드 리팩토링

--- src/test/java/com/example/restexample/index/IndexControllerTest.java
+++ src/test/java/com/example/restexample/index/IndexControllerTest.java
@@ -1,31 +1,13 @@
 package com.example.restexample.index;
 
-import com.example.restexample.common.RestDocsConfiguration;
+import com.example.restexample.common.BaseControllerTest;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
-@AutoConfigureRestDocs
-@Import(RestDocsConfiguration.class)
-@ActiveProfiles("test")
-class IndexControllerTest {
-    @Autowired
-    MockMvc mockMvc;
-
+class IndexControllerTest extends BaseControllerTest {
     @Test
     void index() throws Exception {
         this.mockMvc.perform(get("/api/"))
