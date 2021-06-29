63f5648c352ea77dbb9a01c0dbc67a50ae31732d
Song <stalker5217@gmail.com>
2021-06-29 13:18:21 +0900

    스프링 시큐리티 적용

---
+++ src/main/java/com/example/restexample/accounts/AccountRepository.java
@@ -0,0 +1,11 @@
+package com.example.restexample.accounts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Integer> {
+    Optional<Account> findByEmail(String username);
+}
